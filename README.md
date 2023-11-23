c927a953b19139f4166cc6e22d1ebe5bbd5dc126
DESKTOP-2RG1P79\IT04 <jeffreysing.jsl@gmail.com>
2023-11-23 16:32:05 +0800

    added player have limited number of guess, fix computer keep guessing when correct answer is not give, added README.md file

---
+++ README.md
@@ -0,0 +1,16 @@
+## Mini game
+Mini game develop based on python.
+1. Guessing the correct number
+2. Hangman
+3. Minesweeper
+4. Rock paper scissor
+5. Tic tac toe
+
+## How to use
+1. Clone or donwload the repository.
+2. Open the file with the mini game name.
+3. Select the file and run
+
+## Requirement
+1. [Python](https://www.python.org/)
+2. IDE or code editor like [VSCode](https://code.visualstudio.com/)
--- guessing/guess.py
+++ guessing/guess.py
@@ -1,41 +1,69 @@
 import random
 
 # Guessing game
-def guess(x):
-    random_number = random.randint(1, x)
-    guess = 0
+# By default, player will have 3 tries to guess the number right, you can change it by changing the variable TRIES
+# By default, the max number range is set to 10, you can change it by changing SET_NUM variable
 
+# To play guessing as player, uncomment line 34
+# To play with computer, uncomment line 69
+# To start game, run the python file.
+
+# Variables
+TRIES = 3
+SET_NUM = 10
+
+# Guess the random number
+def guess(max_num, tries):
+    random_number = random.randint(1, max_num)
+    guess = 0
+    
     while guess != random_number:
-        guess = int(input(f'Guess a number between 1 and {x}: '))
+        print(f'You have {tries}❤️  left')
+        guess = int(input(f'Guess a number between 1 and {max_num}: '))
+
         if guess < random_number:
             print('Sorry wrong number, guess too low. Try again.')
         elif guess > random_number:
             print('Sorry wrong number, guess too high. Try again.')
 
-    print(f'You won! The number is {random_number}')
+        tries -= 1
+        if tries == 0:
+            return print('You lose! You get nothing!')
+        
+    print(f'You won! The number is {random_number} 🎉')
 
 
-guess(10)
+# guess(SET_NUM, TRIES)
 
 
 
 # Computer guessing
-def computer_guess(x):
+def computer_guess(max_num, tries):
+    print(f'Please input h for high, l for low, c for correct as feedback.')
+
     low = 1
-    high = x
+    high = max_num
     feedback = ''
-    while feedback != 'c':
-        if low != high:
+    while tries > 0:
+        print(f'Computer have {tries}❤️  left')
+
+        if low <= high:
             guess = random.randint(low, high)
-        else:
-            guess = low
-        feedback = input(f'Is {guess} too high, too low, or correct?').lower()
+        elif low > high:
+            # when low > high, invalid answer is chosen
+            return print('No further number can be guessed, game will be terminated. Are you by any chance cheating? 😜')
+
+        feedback = input(f'Is {guess} too high, too low, or correct? ').lower()
+
         if feedback == 'h':
             high = guess - 1
         elif feedback == 'l':
             low = guess + 1
+        elif feedback == 'c':
+            return print(f'{guess} is correct!🤖 win')
+        tries -= 1
 
-    print(f'{guess} is correct! Computer win')
+    print(f'🤖 failed to guess your number, You win!')
 
-computer_guess(10)
+# computer_guess(SET_NUM, TRIES)
 
